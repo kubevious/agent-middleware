2db6bc9bad3cfc017757a9dab72c7f6a71d15392
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-22 12:17:22 -0700

    exposed report config.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { K8sConfig, K8sApiInfo, K8sApiResourceInfo } from './types/k8s';
 
 export { RequestReportSnapshot, ResponseReportSnapshot, ReportableSnapshotItem } from './reportable/types';
 export { RequestReportSnapshotItems, ResponseReportSnapshotItems } from './reportable/types';
+export { RequestReportConfig } from './reportable/types';
 export { RequestReportChunks, ReportableDataItem } from './reportable/types';
 export { RequestActivateSnapshot, ResponseActivateSnapshot } from './reportable/types';
 
