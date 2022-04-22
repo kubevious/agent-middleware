e986e2652fb2f5d6f7d73b23a9d5381550f4f803
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-22 11:58:35 -0700

    added reportable types.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 export { ItemId, IConcreteRegistry, IConcreteItem, ConcreteRegistryFilter } from './types/registry';
 export { K8sConfig, K8sApiInfo, K8sApiResourceInfo } from './types/k8s';
 
+export { RequestReportSnapshot, ResponseReportSnapshot, ReportableSnapshotItem } from './reportable/types';
+export { RequestReportSnapshotItems, ResponseReportSnapshotItems } from './reportable/types';
+export { RequestReportChunks, ReportableDataItem } from './reportable/types';
+export { RequestActivateSnapshot, ResponseActivateSnapshot } from './reportable/types';
+
 export { extractK8sConfigId } from './utils/registry';
 export { parseConfigApiVersion, parseApiVersion } from './utils/k8s';
